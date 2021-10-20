f7a1bad7593510536e0722834cb4d3e4f6c57f13
Declan Zou <declan.zou@ringcentral.com>
2021-10-21 01:36:53 +0800

    test RenderTarget

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -2,12 +2,19 @@ import { RcTooltip } from "@ringcentral/juno/components/Tooltip";
 import { RcThemeProvider } from "@ringcentral/juno/foundation/theme/ThemeProvider";
 import React from "react";
 
+// import { RenderTarget } from "framer";
 export const Tooltip = ({ _children, children, ...rest }: any) => {
+  // const current = RenderTarget.current();
+
   return (
     <RcThemeProvider>
       {_children.length > 0 ? (
         // because framer issue, use ignorePointer to wrap that
-        <RcTooltip {...rest} ignorePointer>
+        <RcTooltip
+          // open={current === RenderTarget.canvas ? true : open}
+          {...rest}
+          ignorePointer
+        >
           {_children[0].props.children}
         </RcTooltip>
       ) : (
