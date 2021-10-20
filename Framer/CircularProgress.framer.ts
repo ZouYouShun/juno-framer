14bebe70c55d5f18be439599c6c2a13e98e05c5f
Declan Zou <declan.zou@ringcentral.com>
2021-10-21 01:44:56 +0800

    fix

---
+++ Framer/CircularProgress.framer.ts
@@ -0,0 +1,51 @@
+import { addPropertyControls, ControlType } from "framer";
+import * as lib from "http://127.0.0.1:8000/index.js";
+
+addPropertyControls(lib.CircularProgress, {
+  color: {
+    title: "color",
+    type: ControlType.Enum,
+    /** description: "custom color", */
+    defaultValue: "interactive.f01",
+    options: lib.colorOptions,
+  },
+  size: {
+    title: "size",
+    type: ControlType.Number,
+    /** description: "The size of the circle.
+If using a number, the pixel unit is assumed.
+If using a string, you need to provide the CSS unit, e.g '3rem'.", */
+    defaultValue: 24,
+  },
+  thickness: {
+    title: "thickness",
+    type: ControlType.Number,
+    /** description: "The thickness of the circle.", */
+    defaultValue: 4,
+  },
+  variant: {
+    title: "variant",
+    type: ControlType.Enum,
+    /** description: "The variant to use.
+Use indeterminate when there is no progress value.", */
+    defaultValue: 'indeterminate',
+    options: ["static", "determinate", "indeterminate"],
+  },
+  value: {
+    title: "value",
+    type: ControlType.Number,
+    /** description: "The value of the progress indicator for the determinate variant.
+Value between 0 and 100.", */
+    defaultValue: undefined,
+  },
+  disableShrink: {
+    title: "disableShrink",
+    type: ControlType.Boolean,
+    /** description: "If `true`, the shrink animation is disabled.
+This only works if variant is `indeterminate`.", */
+    defaultValue: false,
+  },
+});
+
+const RcCircularProgress: React.ComponentType = lib.CircularProgress;
+export default RcCircularProgress;
--- Framer/LinearProgress.framer.ts
+++ Framer/LinearProgress.framer.ts
@@ -1,26 +1,35 @@
 import { addPropertyControls, ControlType } from "framer";
-import * as lib from "https://zouyoushun.github.io/juno-framer/esmbuild@1.0.7/index.js";
+import * as lib from "http://127.0.0.1:8000/index.js";
 
 addPropertyControls(lib.LinearProgress, {
   color: {
-    type: ControlType.Enum,
     title: "color",
-    defaultValue: "informative.f02",
+    type: ControlType.Enum,
+    /** description: "custom color", */
+    defaultValue: "interactive.f01",
     options: lib.colorOptions,
   },
   variant: {
     title: "variant",
     type: ControlType.Enum,
-    defaultValue: "indeterminate",
+    /** description: "The variant to use.
+Use indeterminate or query when there is no progress value.", */
+    defaultValue: 'indeterminate',
     options: ["determinate", "indeterminate", "buffer", "query"],
   },
   value: {
     title: "value",
     type: ControlType.Number,
+    /** description: "The value of the progress indicator for the determinate and buffer variants.
+Value between 0 and 100.", */
+    defaultValue: undefined,
   },
   valueBuffer: {
     title: "valueBuffer",
     type: ControlType.Number,
+    /** description: "The value for the buffer variant.
+Value between 0 and 100.", */
+    defaultValue: undefined,
   },
 });
 
