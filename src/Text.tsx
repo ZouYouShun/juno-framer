4edcd949c44c4f5d498e497a7fe88dfdbb3898cf
Declan Zou <declan.zou@ringcentral.com>
2021-10-21 10:50:32 +0800

    support titleWhenOverflow

--- src/Text.tsx
+++ src/Text.tsx
@@ -2,10 +2,23 @@ import { RcText } from "@ringcentral/juno/components/Text";
 import { RcThemeProvider } from "@ringcentral/juno/foundation/theme/ThemeProvider";
 import React from "react";
 
-export const Text = ({ _children, startIcon, endIcon, ...rest }: any) => {
+export const Text = ({
+  _children,
+  titleWhenOverflow: titleWhenOverflowProp,
+  ...rest
+}: any) => {
+  let titleWhenOverflow: any = +titleWhenOverflowProp;
+  if (titleWhenOverflowProp === "true") {
+    titleWhenOverflow = true;
+  } else if (titleWhenOverflowProp === "false") {
+    titleWhenOverflow = false;
+  }
+
   return (
     <RcThemeProvider>
-      <RcText {...rest}>{_children}</RcText>
+      <RcText {...rest} titleWhenOverflow={titleWhenOverflow}>
+        {_children}
+      </RcText>
     </RcThemeProvider>
   );
 };
