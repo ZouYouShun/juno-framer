e59580ad1032e481331ee1b494c1cf6dc763e783
Declan Zou <declan.zou@ringcentral.com>
2021-10-22 16:01:42 +0800

    fix accordion

--- src/AccordionDetails.tsx
+++ src/AccordionDetails.tsx
@@ -2,13 +2,19 @@ import { RcAccordionDetails } from "@ringcentral/juno/components/Accordion/Accor
 import { RcThemeProvider } from "@ringcentral/juno/foundation/theme/ThemeProvider";
 import React from "react";
 
-export const AccordionDetails = ({ _children, ...rest }: any) => {
+export const AccordionDetails = ({ _children = [], ...rest }: any) => {
   const children = _children.map((a: any) => {
     return a.props.children || a;
   });
   return (
     <RcThemeProvider>
-      <RcAccordionDetails {...rest}>{children}</RcAccordionDetails>
+      <RcAccordionDetails {...rest}>
+        {children.length > 0 ? (
+          children
+        ) : (
+          <div style={{ background: "white" }}>add children</div>
+        )}
+      </RcAccordionDetails>
     </RcThemeProvider>
   );
 };
